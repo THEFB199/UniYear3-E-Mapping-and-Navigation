23f27956c6c5751d06208dd18233c5ac2ef18930
Freddie Holmes <freddie.holmes@me.com>
2020-02-05 13:08:18 +0000

    sorting out file structure

---
+++ RaspberryPi/Arduino_SPI/Arduino_SPI.ino
@@ -0,0 +1,67 @@
+#include <SPI.h>
+#include <Servo.h>
+uint8_t buf[100];
+uint16_t data[100];
+int idx = 0;
+volatile byte pos;
+volatile bool process_it;
+
+Servo left;
+
+void setup (void)
+{
+  Serial.begin (9600);   // debugging
+  left.attach(2);
+  // turn on SPI in slave mode
+  SPCR |= bit (SPE);
+
+  // have to send on master in, *slave out*
+  pinMode (MISO, OUTPUT);
+
+  // get ready for an interrupt
+  pos = 0;   // buffer empty
+  process_it = false;
+
+  // now turn on interrupts
+  SPI.attachInterrupt();
+
+}  // end of setup
+
+
+// SPI interrupt routine
+ISR (SPI_STC_vect)
+{
+byte c = SPDR;  // grab byte from SPI Data Register
+
+  // add to buffer if room
+  if (pos < sizeof buf)
+    {
+    buf [pos++] = c;
+
+    // time to process buffer
+    if (c == 255)
+      process_it = true;
+
+    }  // end of room available
+}  // end of interrupt routine SPI_STC_vect
+
+// main loop - wait for flag set in interrupt routine
+void loop (void)
+{
+  if (process_it)
+    {
+    buf [pos] = 0;
+    idx = 0;
+    for(int i = 0; i<12; i++){
+    data[idx] = buf[i]*255+buf[i+1];
+    //Serial.println (buf[i]);
+    //Serial.println (data[idx]);
+    idx++;
+    i++;
+    }
+    pos = 0;
+    process_it = false;
+    }  // end of flag set
+  left.write(data[5]);
+  Serial.println (data[5]);
+}  
