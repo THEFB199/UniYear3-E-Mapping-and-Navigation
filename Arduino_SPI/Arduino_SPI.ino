3ae506801affe326e20361fa77e77a2d9bc4b644
Freddie Holmes <freddie.holmes@me.com>
2020-04-28 10:07:22 +0100

    tidy of comments in code

--- Arduino_SPI/Arduino_SPI.ino
+++ Arduino_SPI/Arduino_SPI.ino
@@ -1,71 +1,69 @@
 #include <SPI.h>
 #include <Servo.h>
-uint8_t buf[100];
-uint16_t data[100];
+uint8_t buf[100]; // byte buffer
+uint16_t data[100]; // data buffer to match data being sent
 int idx = 0;
 volatile byte pos;
-volatile bool process_it;
+volatile bool processData;
 
 Servo left;
 Servo right;
-void setup (void)
-{
+void setup (void){
   Serial.begin (9600);   // debugging
   right.attach(1);
   left.attach(2);
   
-  // turn on SPI in slave mode
-  SPCR |= bit (SPE);
+  SPCR |= bit (SPE); // turn on SPI in slave mode
+  pinMode (MISO, OUTPUT); // master in slave out config
 
-  // have to send on master in, *slave out*
-  pinMode (MISO, OUTPUT);
-
-  // get ready for an interrupt
-  pos = 0;   // buffer empty
-  process_it = false;
-
-  // now turn on interrupts
-  SPI.attachInterrupt();
-
-}  // end of setup
+  // initialise data
+  pos = 0;
+  processData = false;
+  SPI.attachInterrupt(); // enable the interrupts
+  
+}
 
 
 // SPI interrupt routine
-ISR (SPI_STC_vect)
-{
-byte c = SPDR;  // grab byte from SPI Data Register
+ISR (SPI_STC_vect){
+byte c = SPDR;  // get byte from SPI Data Register
 
-  // add to buffer if room
-  if (pos < sizeof buf)
+  // add to the buffer if it is not full
+  if (pos < sizeof buf){
+    
+    buf[pos++] = c; // add data to the buffer
+    
+    if (c == 255) // end of the data frame reached so process now in loop
+      processData = true;
+    }
+    
+}  // end of interrupt routine
+
+
+void loop (void){
+  
+  if (processData)
     {
-    buf [pos++] = c;
-
-    // time to process buffer
-    if (c == 255)
-      process_it = true;
-
-    }  // end of room available
-}  // end of interrupt routine SPI_STC_vect
-
-// main loop - wait for flag set in interrupt routine
-void loop (void)
-{
-  if (process_it)
-    {
-    buf [pos] = 0;
+    buf[pos] = 0;
     idx = 0;
-    for(int i = 0; i<12; i++){
-    data[idx] = buf[i]*255+buf[i+1];
-    //Serial.println (buf[i]);
-    //Serial.println (data[idx]);
-    idx++;
-    i++;
+    /*
+    need to combine the data correctly as we recive the data in bytes
+    the data being sent is 16bits so need to shift the MSB's by 8 to make room 
+    for the LSB's
+    */
+    for(int i = 0; i<12; i++){ // expecting 12 bytes of data in buffer
+      data[idx] = buf[i]*255+buf[i+1];
+      idx++;
+      i++;
     }
     pos = 0;
-    process_it = false;
-    }  // end of flag set
+    processData = false;
+    } 
+
+  // Write data to serial for debug and send the timings to the sabertooth
   left.write(data[1]);
   Serial.println (data[1]);
   left.write(data[2]);
   Serial.println (data[2]);
+  
 }  
